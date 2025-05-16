aa4b5760b307bb2958242911bb71a44d6e958969
gudel <gudel@users.noreply.github.com>
2025-05-16 22:36:32 +0700

    feat: add mobile variant to pageUnderConstruction
    add mobile specific image for responsiveness. mark internally as v0.2

--- src/app/components/pageUnderConstruction/pageUnderConstruction.tsx
+++ src/app/components/pageUnderConstruction/pageUnderConstruction.tsx
@@ -2,12 +2,21 @@ import Image from "next/image";
 
 export default function UnderConstruction() {
   return (
+    <>
     <Image
       src="/Under-construction.png"
       alt="Under Construction"
       fill
-      className="z-1 object-cover opacity-50"
+      className="md:block hidden z-1 object-cover opacity-50"
       priority
     />
+    <Image
+    src="/Under-construction-mobile.png"
+    alt="Under Construction"
+    fill
+    className="md:hidden block z-1 object-cover opacity-50"
+    priority
+  />
+  </>
   );
 }
