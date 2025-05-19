3579667c89d430ccea646a123aceb35f5d38693d
gudel <gudel@users.noreply.github.com>
2025-05-19 20:11:45 +0700

    fix: append payload to /about page
    follow up to previous deploy for proper component linking.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,20 @@
-
+import UnderConstruction from "../components/pageUnderConstruction/pageUnderConstruction"
 export default function Product() {
    return(
-    <div className="flex justify min-h-dvh overflow-hidden">
-        <main className="flex">
-        <section className="flex w-20 border-black border-2 ">
+        <div className="fixed  left-0 w-full h-screen">
+{/*        <aside className="fixed h-screen max-h-[calc(100dvh-12.5rem)]">
+       <section className="flex  justify-center h-full w-20 bottom-16 border-white border-2 ">
         <ul>
         <li><button>a</button></li>
         <li><button>b</button></li>
         </ul></section>
-        <section className="flex justify-center place-items-center">
+        </aside> */}
+        <main className="flex justify-center">
+{/*        <section className="flex justify-center place-items-center">
                 <p>I&apos;m cooked, chat.</p>
-        </section>
+        </section> */}
+        <UnderConstruction />
         </main>
-    </div>
+        </div>
    )
 }
